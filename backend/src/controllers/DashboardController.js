3f56d8dcec847a852aafd5da5d6206393c971121
Luiz504 <luiz504@hotmail.com>
2019-10-06 19:37:40 -0300

    feat: virtual img url/ dashboar validation

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -1,8 +1,16 @@
 import Spot from '../models/Spot';
 
+import User from '../models/User';
+
 class DashboardController {
   async show(req, res) {
     const { user_id } = req.headers;
+
+    const userCheck = await User.find({ user: user_id });
+
+    if (!userCheck) {
+      return res.status(401).json({ Error: 'User not found' });
+    }
 
     const spots = await Spot.find({ user: user_id });
 
--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,25 +1,36 @@
 import mongoose from 'mongoose';
 
-const SpotSchema = new mongoose.Schema({
-  thumbnail: {
-    type: String,
+const SpotSchema = new mongoose.Schema(
+  {
+    thumbnail: {
+      type: String,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    techs: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  company: {
-    type: String,
-    required: true,
-  },
-  techs: {
-    type: [String],
-    required: true,
-  },
-  price: {
-    type: Number,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 export default mongoose.model('Spot', SpotSchema);
