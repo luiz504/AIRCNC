69ed612293857bf607f961f087cb3e38de857c34
Luiz504 <luiz504@hotmail.com>
2019-10-07 10:40:15 -0300

    Base mobile app / backend ignore upload files

---
+++ mobile/src/pages/Book/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Book() {
+  return (
+    <SafeAreaView>
+      <Text>NewSpot</Text>
+    </SafeAreaView>
+  );
+}
---
+++ mobile/src/pages/List/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function List() {
+  return (
+    <SafeAreaView>
+      <Text>Dashboard</Text>
+    </SafeAreaView>
+  );
+}
